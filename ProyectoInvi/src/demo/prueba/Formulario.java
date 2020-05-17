7269549e115c39d471bbee57e74caceaa8c0712a
pierodiaz <pierodiazxd@gmail.com>
2020-05-17 00:27:00 -0500

    git Formulario

---
+++ ProyectoInvi/src/demo/prueba/Formulario.java
@@ -0,0 +1,14 @@
+package demo.prueba;
+
+public class Formulario {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Invitado 1");
+		System.out.println("Invitado 2");
+		System.out.println("Invitado 3/2");
+
+
+	}
+
+}
